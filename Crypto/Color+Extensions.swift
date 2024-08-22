2b9bd70ff0c089e3cad08aaf5fc8327eaa4475bc
Ryan Rouxinol <ryanrouxinol17@gmail.com>
2024-08-22 20:20:01 -0300

    first commit

--- Crypto/Color+Extensions.swift
+++ Crypto/Color+Extensions.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+import UIKit
+
+extension UIColor {
+    static let bgDarkModeAsset = UIColor(named: "BackgroundDarkModeColor")
+    static let YellowAsset = UIColor(named: "YellowAsset")
+    static let softGrayAsset = UIColor(named: "TextGreyAsset")
+    static let GreyAsset = UIColor(named: "GreyAsset")
+    static let RedAsset = UIColor(named: "RedAsset")
+    static let GreenAsset = UIColor(named: "GreenAsset")
+    static let WhiteAsset = UIColor(named: "WhiteAsset")
+}
