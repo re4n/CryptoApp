66b227f895e7d979317049bca6b0656327d25d79
Ryan Rouxinol <ryanrouxinol17@gmail.com>
2024-08-22 20:31:04 -0300

    reset ViewController

--- Crypto/ViewController.swift
+++ Crypto/ViewController.swift
@@ -11,26 +11,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor  = .backgroundDarkMode
-        let service = CryptoService()
-        service.fetchCryptos { cryptos in
-            if let cryptos = cryptos {
-                for crypto in cryptos {
-                    let formattedChangePercent = self.formatPercentage(crypto.changePercent24Hr)
-                    let formattedChangePrice = self.formatPercentage(crypto.priceUsd)
-                    print("Nome: \(crypto.name), Preço: \(formattedChangePrice), Variação 24h: \(formattedChangePercent)%")
-                }
-            } else {
-                print("Nenhuma criptomoeda encontrada.")
-            }
-        }
-    }
-
-    func formatPercentage(_ value: String) -> String {
-        if let doubleValue = Double(value) {
-            return String(format: "%.2f", doubleValue)
-        } else {
-            return value
-        }
     }
 }
+
